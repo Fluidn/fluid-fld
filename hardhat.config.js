b19f3dab6cb73fa47bcba44031cb48344bdef7d4
peterndiritu <peternngendo@gmail.com>
2025-11-08 13:50:11 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,15 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
+  defaultNetwork: "polygon",
   networks: {
     polygon: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      url: process.env.ALCHEMY_POLYGON_URL,
+      accounts: [process.env.PRIVATE_KEY]
     },
-    mumbai: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-    },
-  },
+    mumbai: { // testnet
+      url: process.env.ALCHEMY_POLYGON_URL.replace("polygon-mainnet", "polygon-mumbai"),
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
